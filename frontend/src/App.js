bee7e7a0f997f7602397023f43f82e7f4b6c218a
adityakaklij <90379168+adityakaklij@users.noreply.github.com>
2022-06-02 17:40:27 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,46 @@
+
+import './App.css';
+import React from 'react';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import Mint from './components/Mint'
+import Footer from './components/Footer';
+
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+function App() {
+
+
+  return (
+
+         <Router basename='/'>
+    <div className="App">
+       <Navbar/>
+
+      <Switch>
+
+          <Route exact path="/">
+              <Home/>
+          </Route>
+          <Route exact path="/Home">
+              <Home/>
+          </Route>
+
+          <Route exact path="/Mint">
+              <Mint/>
+          </Route>
+        </Switch>
+
+      <Footer/>
+
+    </div>
+    </Router>
+  )
+}
+
+export default App;
